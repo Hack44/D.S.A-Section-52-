50ed5fcf666b50d15229f10091b256d8ed44622f
Harsh Verma <royalharsh2102@gmail.com>
2025-10-10 08:12:54 +0530

    Max. value in the array

---
+++ maxarr.java
@@ -0,0 +1,13 @@
+//print maximum no in arr 
+public class maxarr {
+    public static void main(String [] args){
+        int [] arr= {10,20,30,40,50};
+        int max= arr[0];
+        for (int i=1; i<arr.length; i++){
+            if (arr[i]>max){
+              max=arr[i]; 
+            } 
+        }
+        System.out.print(max);
+    }
+}
