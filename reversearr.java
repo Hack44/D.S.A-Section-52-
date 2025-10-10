15d9ad08825309c6b1c38d3fc59fa1c35bf6e160
Harsh Verma <royalharsh2102@gmail.com>
2025-10-10 08:17:05 +0530

    Reverse the provided array

---
+++ reversearr.java
@@ -0,0 +1,8 @@
+class reversearr{
+    public static void main(String []args){
+        int [] arr= {100,200,300,400,500};
+        for( int i= arr.length-1; i>-1; i--){
+        System.out.println(arr[i]+ " ");
+    }
+}
+}
