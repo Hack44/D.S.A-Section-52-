9c91a962d1449f664f92e75ed4bbd01b40e0b7ef
Harsh Verma <royalharsh2102@gmail.com>
2025-10-14 08:29:17 +0530

    Tower of hanoi

---
+++ TowerOfHanoi.java
@@ -0,0 +1,24 @@
+public class TowerOfHanoi {
+
+    public static void solve(int n, char fromRod, char toRod, char auxRod) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from rod " + fromRod + " to rod " + toRod);
+            return;
+        }
+
+        solve(n - 1, fromRod, auxRod, toRod);
+       
+        System.out.println("Move disk " + n + " from rod " + fromRod + " to rod " + toRod);
+      
+        solve(n - 1, auxRod, toRod, fromRod);
+    }
+
+    public static void main(String[] args) {
+
+        int numOfDisks = 3; 
+        
+        System.out.println("Steps to solve Tower of Hanoi with " + numOfDisks + " disks:");
+
+        solve(numOfDisks, 'A', 'C', 'B');
+    }
+}
